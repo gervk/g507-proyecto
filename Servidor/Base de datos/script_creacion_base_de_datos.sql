69c3fd9a6f3bda33313fe16a443a66f37b8d2754
Sebastian Lezama <sml.seba@gmail.com>
2016-09-26 14:36:05 -0300

    script DB

--- Servidor/Base de datos/script_creacion_base_de_datos.sql
+++ Servidor/Base de datos/script_creacion_base_de_datos.sql
@@ -7,6 +7,8 @@ CREATE TABLE IF NOT EXISTS usuario(
 	codigo_arduino INTEGER NOT NULL UNIQUE,
 	id_serv_agua INTEGER NOT NULL,
 	id_serv_luz INTEGER NOT NULL,
+	created_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NOT NULL,
 	PRIMARY KEY (user_name)
 );
 
@@ -15,11 +17,15 @@ CREATE TABLE IF NOT EXISTS pregunta_seguridad(
 	user_name varchar(20) NOT NULL,
 	pregunta varchar(255),
 	respuesta varchar(255),
+	created_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NOT NULL,
 	PRIMARY KEY (id, user_name)
 );
 
 CREATE TABLE IF NOT EXISTS arduino(
 	codigo INTEGER NOT NULL PRIMARY KEY
+	created_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NOT NULL,
 );
 
 CREATE TABLE IF NOT EXISTS consumo(
@@ -27,24 +33,31 @@ CREATE TABLE IF NOT EXISTS consumo(
 	codigo_arduino INTEGER NOT NULL,	
 	tipo smallint NOT NULL,
 	consumo double precision,
-	fecha TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	created_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NOT NULL,
 	PRIMARY KEY (id, codigo_arduino, tipo)
 );
 
 CREATE TABLE IF NOT EXISTS tipo_consumo(
 	id smallint NOT NULL PRIMARY KEY,
 	descripcion varchar(20)
+	created_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NOT NULL,
 );
 
 CREATE TABLE IF NOT EXISTS servicio_luz(
 	id INTEGER NOT NULL PRIMARY KEY,
 	id_empresa INTEGER NOT NULL,
 	id_tarifa INTEGER NOT NULL
+	created_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NOT NULL,
 );
 
 CREATE TABLE IF NOT EXISTS empresa_luz(
 	id INTEGER NOT NULL PRIMARY KEY,
 	nombre varchar(50)
+	created_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NOT NULL,
 );
 
 CREATE TABLE IF NOT EXISTS tarifa_luz(
@@ -53,5 +66,7 @@ CREATE TABLE IF NOT EXISTS tarifa_luz(
 	id_empresa INTEGER NOT NULL,
 	cargo_fijo double precision,
 	cargo_variable double precision
+	created_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NOT NULL,
 );
 
