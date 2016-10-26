549ef4b3b50bbe38746b57638ac7e30a4cf5b23a
flormarcaccio <flormarcaccio@gmail.com>
2016-10-26 12:43:50 -0300

    Recomendaciones - solo falta que la notificacion te lleve al fragment

--- Aplicacion/app/src/main/java/g507/controldeconsumo/RecomendAguaFragment.java
+++ Aplicacion/app/src/main/java/g507/controldeconsumo/RecomendAguaFragment.java
@@ -6,7 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,6 +21,7 @@ public class RecomendAguaFragment extends Fragment {
 
 
     private View view;
+    private ListView listViewRecAgua;
 
     public RecomendAguaFragment() {
         // Required empty public constructor
@@ -30,6 +37,14 @@ public class RecomendAguaFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_recomend_agua, container, false);
+
+        listViewRecAgua = (ListView)view.findViewById(R.id.lvRecomendAgua);
+
+        ArrayList<String> recomendAguaList = new ArrayList<String>(Arrays.asList(getResources().getStringArray(R.array.string_array_rec_agua)));
+
+        ListAdapter listAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_recomend, recomendAguaList);
+
+        listViewRecAgua.setAdapter(listAdapter);
 
         return view;
     }
--- Aplicacion/app/src/main/java/g507/controldeconsumo/RecomendLuzFragment.java
+++ Aplicacion/app/src/main/java/g507/controldeconsumo/RecomendLuzFragment.java
@@ -6,7 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,7 +20,7 @@ import android.widget.TextView;
 public class RecomendLuzFragment extends Fragment {
 
     private View view;
-
+    private ListView listViewRecLuz;
 
     public RecomendLuzFragment() {
         // Required empty public constructor
@@ -29,6 +35,14 @@ public class RecomendLuzFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_recomend_luz, container, false);
+
+        listViewRecLuz = (ListView)view.findViewById(R.id.lvRecomendLuz);
+
+        ArrayList<String> recomendLuzList = new ArrayList<String>(Arrays.asList(getResources().getStringArray(R.array.string_array_rec_luz)));
+
+        ListAdapter listAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_recomend, recomendLuzList);
+
+        listViewRecLuz.setAdapter(listAdapter);
 
         return view;
     }
